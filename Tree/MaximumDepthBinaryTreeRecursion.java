8532e3fcb11a1209e7b5b046453f6c5f0d9b1cd7
khanh <khanhtranjps15@gmail.com>
2025-10-04 23:55:14 +0700

    tree

---
+++ Tree/MaximumDepthBinaryTreeRecursion.java
@@ -0,0 +1,11 @@
+package Tree;
+
+public class MaximumDepthBinaryTreeRecursion {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
